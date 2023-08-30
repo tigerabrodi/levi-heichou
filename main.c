abe122fa00f2472bb1a42bd1091a37b53db88b0d
Tiger Abrodi <tigerabrodi@gmail.com>
2023-08-30 08:50:18 +0200

    commit

--- main.c
+++ main.c
@@ -1,44 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  // For standard input-output functions
+#include <stdlib.h> // For memory allocation and random number generation
+#include <string.h> // For string manipulation functions like strlen
+#include <time.h>   // For time-related functions (used for random seed)
 
-// This is typing practice program
-
+// Declare arrays of subjects, verbs, and objects for random sentence generation
 const char *subjects[] = {"The cat", "A boy", "She", "John"};
 const char *verbs[] = {"jumps", "runs", "plays", "reads"};
 const char *objects[] = {"the book", "a ball", "over the fence", "fast"};
 
-int get_arr_length(char *array)
-{
-	return sizeof(array) / sizeof(array[0]);
-}
-
 char *create_random_sentence()
 {
-	char *sentence = malloc(get_arr_length(subjects) + get_arr_length(verbs) + get_arr_length(objects) + 1);
-	int randomIndexForSubjects = rand() % get_arr_length(subjects);
-	int randomIndexForVerbs = rand() % get_arr_length(verbs);
-	int randomIndexForObjects = rand() % get_arr_length(objects);
+	// Generate random index to pick subject, verb, and object
+	int randomIndexForSubjects = rand() % 4; // rand() returns a random integer
+	int randomIndexForVerbs = rand() % 4;
+	int randomIndexForObjects = rand() % 4;
 
+	char *sentence = malloc(50);
+
+	// Create a random sentence using sprintf
 	sprintf(sentence, "%s %s %s", subjects[randomIndexForSubjects], verbs[randomIndexForVerbs], objects[randomIndexForObjects]);
 
+	// Return the created random sentence
 	return sentence;
 }
 
+// Function to calculate typing accuracy
 double calculateAccuracy(char *original, char *input)
 {
+	// Get lengths of original sentence and user input
 	int lengthOriginal = strlen(original);
 	int lengthInput = strlen(input);
 	int errors = 0;
 
-	// If the lengths differ, add the difference to errors
+	// If lengths are different, it's an error
 	if (lengthOriginal != lengthInput)
 	{
 		errors += abs(lengthOriginal - lengthInput);
 	}
 
-	// Iterate over the smallest length
+	// Choose smaller length to iterate through characters
 	int lengthToIterate = lengthOriginal < lengthInput ? lengthOriginal : lengthInput;
 
+	// Loop through characters and count errors
 	for (int i = 0; i < lengthToIterate; i++)
 	{
 		if (original[i] != input[i])
@@ -47,49 +50,59 @@ double calculateAccuracy(char *original, char *input)
 		}
 	}
 
-	int totalCharacters = lengthOriginal; // Total characters from the original sentence
+	// Calculate accuracy as percentage
+	int totalCharacters = lengthOriginal;
 	return 100.0 * (totalCharacters - errors) / totalCharacters;
 }
 
+// Main function
 int main()
 {
+	srand(time(0)); // Seed the random number generator with current time
 
-	int randomIndexForSubjects = rand() % (sizeof(subjects) / sizeof(subjects[0]));
-	int randomIndexForVerbs = rand() % (sizeof(verbs) / sizeof(verbs[0]));
-	int randomIndexForObjects = rand() % (sizeof(objects) / sizeof(objects[0]));
-
+	// Initialize variables for keeping track of overall accuracy
 	double total_accuracy = 0;
+	int rounds_played = 0;
 
+	// Print welcome message
 	printf("Welcome to Levi!\n");
-	printf("This is typing practice game.\n");
-	printf("We will calculate your accuracy throughout your tries.\n");
-	printf("A random sentence will be generated and you've to under the sentence as fast as you can.\n");
-	printf("If you enter 'end' the program will exit and display your score.\n");
-	printf("If you enter 'score' the program will display your score.\n");
+	printf("This is a typing practice game.\n");
 
+	// Infinite loop for game
 	while (1)
 	{
+		// Generate a random sentence
 		char *random_sentence = create_random_sentence();
-		printf("Enter the sentence as fast as you can: %s", random_sentence);
-		int user_sentence_length = get_arr_length(subjects) + get_arr_length(verbs) + get_arr_length(objects) + 1;
-		char user_sentence[user_sentence_length];
-		fgets(user_sentence, user_sentence_length, stdin);
 
-		if (user_sentence == "end")
+		// Display the random sentence to the user
+		printf("Enter the sentence as fast as you can: %s\n", random_sentence);
+
+		char user_sentence[256];
+		fgets(user_sentence, 256, stdin);
+
+		user_sentence[strcspn(user_sentence, "\n")] = 0;
+
+		if (strcmp(user_sentence, "end") == 0)
 		{
-			printf("Your accuracy score is: %f", total_accuracy);
-			printf("Thanks for playing!");
+			printf("Your final accuracy score is: %f\n", total_accuracy / rounds_played);
+			printf("Thanks for playing!\n");
+			free(random_sentence);
 			break;
 		}
 
-		if (user_sentence == "score")
+		if (strcmp(user_sentence, "score") == 0)
 		{
-			printf("Your accuracy score is: %f", total_accuracy);
-			continue;
+			printf("Your current accuracy score is: %f\n", total_accuracy / rounds_played);
+		}
+		else
+		{
+			double accuracy = calculateAccuracy(random_sentence, user_sentence);
+			total_accuracy += accuracy;
+			rounds_played++;
 		}
 
-		// Here we calculate accuracy
-		total_accuracy += calculateAccuracy(random_sentence, user_sentence);
+		// Free the allocated memory for the random sentence
+		free(random_sentence);
 	}
 
 	return 0;
